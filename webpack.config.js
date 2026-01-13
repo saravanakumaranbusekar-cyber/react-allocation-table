656e8f5b320b153a724a40a5fceb579692bf769e
Saravanakumar <saravanakumaranbusekar@gmail.com>
2026-01-13 17:42:27 +0530

    Add Netlify redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
+
   entry: "./src/index.tsx",
 
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "react-allocation-table",
-
+    filename: "bundle.[contenthash].js",
+    publicPath: "/",
     clean: true
-  },
-
-  devServer: {
-    static: "./public",
-    port: 3000,
-    open: true,
-    hot: true
   },
 
   resolve: {
@@ -35,5 +29,11 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
 };
